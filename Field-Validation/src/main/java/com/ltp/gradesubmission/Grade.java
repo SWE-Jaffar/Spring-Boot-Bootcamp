f6e5f562c6cfabf16a2742582745e4a909064507
SWE-Jaffar <Jaffar4k@yahoo.com>
2023-11-28 01:53:01 +0300

    Validate the name & subject

--- Field-Validation/src/main/java/com/ltp/gradesubmission/Grade.java
+++ Field-Validation/src/main/java/com/ltp/gradesubmission/Grade.java
@@ -2,9 +2,16 @@ package com.ltp.gradesubmission;
 
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+
 public class Grade {
+    
+    @NotBlank(message = "Name is mandatory")
     private String name;
+
+    @NotBlank(message = "Subject is mandatory")
     private String subject;
+
     private String score;
     private String id;
 
