fa6b574873661c45cfeb911b7adab9fb3e64112a
SWE-Jaffar <Jaffar4k@yahoo.com>
2023-11-29 01:45:12 +0300

    Made the objects as beans.

--- Beans/src/main/java/com/ltp/gradesubmission/controller/GradeController.java
+++ Beans/src/main/java/com/ltp/gradesubmission/controller/GradeController.java
@@ -2,20 +2,20 @@ package com.ltp.gradesubmission.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.ltp.gradesubmission.Grade;
 import com.ltp.gradesubmission.service.GradeService;
-
 @Controller
 public class GradeController {
-
-    GradeService gradeService = new GradeService();
+    
+    @Autowired
+    GradeService gradeService;
 
     @GetMapping("/")
     public String getForm(Model model, @RequestParam(required = false) String id) {
