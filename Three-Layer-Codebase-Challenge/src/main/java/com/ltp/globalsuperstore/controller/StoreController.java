3a9e9df7c9e25b15ecd7f5e0f32325c255825bb2
SWE-Jaffar <Jaffar4k@yahoo.com>
2023-11-29 00:51:03 +0300

    Moved the logic to the service class | Solved | My solutions

--- Three-Layer-Codebase-Challenge/src/main/java/com/ltp/globalsuperstore/controller/StoreController.java
+++ Three-Layer-Codebase-Challenge/src/main/java/com/ltp/globalsuperstore/controller/StoreController.java
@@ -1,6 +1,4 @@
 package com.ltp.globalsuperstore.controller;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,20 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.ltp.globalsuperstore.Constants;
 import com.ltp.globalsuperstore.Item;
-import com.ltp.globalsuperstore.repositery.StoreRepo;
+import com.ltp.globalsuperstore.service.StoreService;
 
 @Controller
 public class StoreController {
 
-    StoreRepo storeRepo = new StoreRepo();
+    StoreService storeService = new StoreService();
 
     @GetMapping("/")
     public String getForm(Model model, @RequestParam(required = false) String id) {
-        int index = storeRepo.getIndexFromId(id);
-        model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : storeRepo.getItemIndex(index));
+        int index = storeService.getIndexFromId(id);
+        model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : storeService.getItemIndex(index));
         return "form";
     }
 
@@ -32,37 +29,22 @@ public class StoreController {
             result.rejectValue("price", "", "Price cannot be less than discount");
         }
         if (result.hasErrors()) return "form";
-        int index = getIndexFromId(item.getId());
-        String status = Constants.SUCCESS_STATUS;
-        if (index == Constants.NOT_FOUND) {
-            storeRepo.addItem(item);
-        } else if (within5Days(item.getDate(), storeRepo.getItemIndex(index).getDate())) {
-            storeRepo.setItem(index, item);
-        } else {
-            status = Constants.FAILED_STATUS;
-        }
-        redirectAttributes.addFlashAttribute("status", status);
+        
+        storeService.submitItem(item);
+
+        redirectAttributes.addFlashAttribute("status", storeService.getStatus());
         return "redirect:/inventory";
     }
 
     @GetMapping("/inventory")
     public String getInventory(Model model) {
-        model.addAttribute("items", storeRepo.getItems());
+        model.addAttribute("items", storeService.getItems());
         return "inventory";
     }
 
-    public int getIndexFromId(String id) {
-        for (int i = 0; i < storeRepo.getItems().size(); i++) {
-            if (storeRepo.getItemIndex(i).getId().equals(id)) return i;
-        }
-        return Constants.NOT_FOUND;
-    }
+    
 
-    public boolean within5Days(Date newDate, Date oldDate) {
-        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
-        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
-    }
-
+   
 
 
 }
--- Three-Layer-Codebase-Challenge/src/main/java/com/ltp/globalsuperstore/service/StoreService.java
+++ Three-Layer-Codebase-Challenge/src/main/java/com/ltp/globalsuperstore/service/StoreService.java
@@ -1,5 +1,66 @@
 package com.ltp.globalsuperstore.service;
 
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.ltp.globalsuperstore.Constants;
+import com.ltp.globalsuperstore.Item;
+import com.ltp.globalsuperstore.repositery.StoreRepo;
+
 public class StoreService {
+    StoreRepo storeRepo = new StoreRepo();
+
+   
+    public Item getItemIndex(int index) {
+        return storeRepo.getItemIndex(index);
+    }
+
+    public void addItem(Item item) {
+        storeRepo.addItem(item);
+    }
+
+    public Item setItem(int index, Item item) {
+        return storeRepo.setItem(index, item);
+    }
+
+    public List<Item> getItems() {
+        return storeRepo.getItems();
+    }
+
+     public int getIndexFromId(String id) {
+        for (int i = 0; i < storeRepo.getItems().size(); i++) {
+            if (storeRepo.getItemIndex(i).getId().equals(id)) return i;
+        }
+        return Constants.NOT_FOUND;
+    }
+
+     public boolean within5Days(Date newDate, Date oldDate) {
+        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
+    }
     
+    String status = Constants.SUCCESS_STATUS;
+    
+    public void submitItem(Item item){
+        int index = getIndexFromId(item.getId());
+        if (index == Constants.NOT_FOUND) {
+            addItem(item);
+        } else if (within5Days(item.getDate(), getItemIndex(index).getDate())) {
+            setItem(index, item);
+        } else {
+            setStatus(Constants.FAILED_STATUS); 
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+
 }
