d4faa9bfc10e68a9758d547615821c67a1fa6bde
SWE-Jaffar <Jaffar4k@yahoo.com>
2023-11-29 03:32:36 +0300

    Set up the integration test class

--- Testing/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
+++ Testing/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
@@ -1,13 +1,29 @@
 package com.ltp.gradesubmission;
 
+import static org.junit.Assert.assertNotNull;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.ltp.gradesubmission.controller.GradeController;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GradeSubmissionApplicationTests {
+
+	@Autowired
+	private GradeController gradeController;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(gradeController);
+		assertNotNull(mockMvc);
 	}
 
 }
