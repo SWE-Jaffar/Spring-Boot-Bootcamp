24c154144013666337c0bdc33bdcf14d54049486
SWE-Jaffar <Jaffar4k@yahoo.com>
2023-11-29 05:34:17 +0300

    Update GradeSubmissionApplicationTests.java

--- Testing/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
+++ Testing/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
@@ -2,12 +2,13 @@ package com.ltp.gradesubmission;
 
 import static org.junit.Assert.assertNotNull;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import com.ltp.gradesubmission.controller.GradeController;
 
 @SpringBootTest
@@ -24,6 +25,36 @@ class GradeSubmissionApplicationTests {
 	void contextLoads() {
 		assertNotNull(gradeController);
 		assertNotNull(mockMvc);
+	}
+
+	@Test
+	public void testShowGradeForm(){
+		RequestBuilder req = MockMvcRequestBuilders.get("/?id=123");
+		try {
+			mockMvc.perform(req).andExpect(status().is2xxSuccessful())
+			.andExpect(view().name("form"))
+			.andExpect(model().attributeExists("grade"))
+			;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testSuccessfulSubmission(){
+		RequestBuilder req = MockMvcRequestBuilders.post("/handleSubmit")
+		.param("id", "123")
+		.param("name", "Ali")
+		.param("subject", "Math")
+		.param("score", "C")
+		;
+		try {
+			mockMvc.perform(req).andExpect(status().is3xxRedirection())
+			.andExpect(view().name("redirect:/grades"))
+			;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
