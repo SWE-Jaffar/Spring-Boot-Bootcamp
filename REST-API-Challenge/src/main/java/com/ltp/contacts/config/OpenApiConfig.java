12ae22b5a0830a16c45de1590912b58f40978323
SWE-Jaffar <Jaffar4k@yahoo.com>
2023-12-01 03:13:01 +0300

    Documented the Contact API

--- REST-API-Challenge/src/main/java/com/ltp/contacts/config/OpenApiConfig.java
+++ REST-API-Challenge/src/main/java/com/ltp/contacts/config/OpenApiConfig.java
@@ -1,16 +1,19 @@
 package com.ltp.contacts.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class OpenApiConfig {
     
     @Bean // Bean para configurar o Swagger
     public  OpenAPI openAPI(){
-        return new OpenAPI();
+        return new OpenAPI()
+        .info(new Info()
+        .title("Contacts API")
+        .description("An API to manage contacts")
+        .version("1.0.0"));
     }
 
 
--- REST-API-Challenge/src/main/java/com/ltp/contacts/web/ContactController.java
+++ REST-API-Challenge/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -16,24 +16,36 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name = "Content Controller", description = "Create and retrieve contacts")
 public class ContactController {
     
     @Autowired
     private ContactService contactService;
-
+    
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of contacts", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Contact.class))))
+    @Operation(summary = "Retrieves contacts", description = "Provides a list of all contacts")
     @GetMapping(value = "/contact/all")
     public ResponseEntity<List<Contact>> getContacts() {
         List<Contact> contacts = contactService.getContacts();
         return new ResponseEntity<>(contacts, HttpStatus.OK);
     }
-
+    
+    @Operation(summary = "Get contact by Id", description = "Returns a contact based on an ID")
     @GetMapping(value = "/contact/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable String id) {
         Contact contact = contactService.getContactById(id);
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
 
+    @Operation(summary = "Create Contact", description = "Creates a contact from the provided payload")
     @PostMapping(value = "/contact")
     public ResponseEntity<Contact> createContact(@Valid @RequestBody Contact contact) {
         contactService.saveContact(contact);
