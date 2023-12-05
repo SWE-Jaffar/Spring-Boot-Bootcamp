8e1a2fe8e4f110fcbd6c9f7af644944df6f0dfae
SWE-Jaffar <Jaffar4k@yahoo.com>
2023-12-05 10:55:32 +0300

    One to Many (Student to grades)

--- Spring-JPA/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
+++ Spring-JPA/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
@@ -23,10 +23,10 @@ public class GradeSubmissionApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Student[] students = new Student[] {
-            new Student(1L, "Harry Potter", LocalDate.of(1980, 7, 31)),
-            new Student(2L, "Ron Weasley", LocalDate.of(1980, 3, 1)),
-            new Student(3L, "Hermione Granger", LocalDate.of(1979, 9, 19)),
-            new Student(4L, "Neville Longbottom", LocalDate.of(1980, 7, 30))
+            new Student("Harry Potter", LocalDate.of(1980, 7, 31)),
+            new Student("Ron Weasley", LocalDate.of(1980, 3, 1)),
+            new Student("Hermione Granger", LocalDate.of(1979, 9, 19)),
+            new Student("Neville Longbottom", LocalDate.of(1980, 7, 30))
         };
 
         for (Student student : students) {
--- Spring-JPA/src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ Spring-JPA/src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -1,32 +1,46 @@
 package com.ltp.gradesubmission.entity;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "student")
 @Getter
 @Setter
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class Student {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+    @NonNull
     @Column(name = "name", nullable = false)
     private String name;
 
+    @NonNull
     @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
+    //one student to many grades
+    @JsonIgnore
+    @OneToMany(mappedBy = "student")
+    private List<Grade> grades;
 }
