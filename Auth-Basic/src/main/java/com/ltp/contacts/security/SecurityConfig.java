d70ee1ba4d923a2e88dcefc16547231969e394b3
SWE-Jaffar <Jaffar4k@yahoo.com>
2023-12-07 15:22:45 +0300

    Update SecurityConfig.java

--- Auth-Basic/src/main/java/com/ltp/contacts/security/SecurityConfig.java
+++ Auth-Basic/src/main/java/com/ltp/contacts/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -22,18 +23,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-         
+
         http
-        .csrf().disable()
-         .authorizeRequests()
-         .antMatchers(HttpMethod.DELETE).hasRole("ADMIN")
-         .antMatchers(HttpMethod.POST).hasAnyRole("ADMIN","USER")
-         .antMatchers(HttpMethod.GET).permitAll()
-         .anyRequest().authenticated()
-         .and()
-         .httpBasic()
-         .and()
-         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(requests -> requests
+                        .antMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                        .antMatchers(HttpMethod.POST).hasAnyRole("ADMIN", "USER")
+                        .antMatchers(HttpMethod.GET).permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
          return http.build();
     }
